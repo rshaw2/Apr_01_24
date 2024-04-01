8dc3a26c9b1a0073873d9f310c3c01b4eda90197
Rahul Kumar Shaw <69197147+rshaw2@users.noreply.github.com>
2024-04-01 18:28:06 +0530

    Upload files from codezen

---
+++ API/Filter/JsonHelperDeserialize.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Text.Json;
+
+namespace Apr0124.Filter
+{
+    public static class JsonHelper
+    {
+        public static T Deserialize<T>(string json)
+        {
+            return JsonSerializer.Deserialize<T>(json);
+        }
+    }
+}
